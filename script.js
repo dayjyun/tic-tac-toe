402938c6a8542342ec951d315dc800021bce9d94
Kevin Barrios <kevinbarrios@ymail.com>
2023-03-29 04:07:33 -0500

    updates on html and pseudo code js file

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
 console.log('JS Loaded')
+// Have an object to store 
+
 // loop through table divArr
 // mouseover event for the block to highlight a background color
-    // color will be based on player's turn
-    // when clicked, 
+    // background color will be based on player's turn
+    // when clicked, player's mark will be placed on div
+
+// grab player 1 box
+// add an event listener when clicked, it opens up a form
+    // the form allows users to update the name
+    // when pressing enter or clicking away, it save the name
+
+// grab player 2 icon
+// add event listener when clicked, it toggles to live person play
+
+
+// Extra
+// Local Storage?
+// Hold name values in a players object
+// if name exists, then populate score values for player 1 and player 2
+// else create a new instance of the player in the players object
+
+    // IF THERE'S LOCAL STORAGE
+    // Create a user icon in the nav bar.
+    // This loads all the saved names for player two
