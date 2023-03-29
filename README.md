316e757d50ab30fd37c55b6640a4318dcf7fbff2
Kevin Barrios <kevinbarrios@ymail.com>
2023-03-29 03:09:44 -0500

    any updates

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # tic-tac-toe
+
+## Future
+More fun along the way!
+- [ ] The user choose to go second
+- [ ] Player can personalize their background color
--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 console.log('JS Loaded')
+// loop through table divArr
+// mouseover event for the block to highlight a background color
+    // color will be based on player's turn
+    // when clicked, 
