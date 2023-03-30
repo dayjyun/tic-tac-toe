88b144c53d60b28e652e923b8b93d800155bf724
Kevin Barrios <kevinbarrios@ymail.com>
2023-03-29 20:55:19 -0500

    current progress with code

--- script.js
+++ script.js
@@ -12,44 +12,42 @@ const playerInfoList = document.querySelectorAll(".player-info");
 
 
 // Header
-// Add event listener to clear out board values
 const newGameButton = document.querySelector(".new-game-button");
 newGameButton.addEventListener("click", () => {
-  // clear out the x and circle classes inside the tables
   tdTags.forEach(tdTag => {
-    tdTag.style.background = 'white'
+    tdTag.classList.remove('lightBlue')
+    tdTag.classList.remove('lightOrange')
   })
 });
 
 // Main
 const turnNameDisplay = document.querySelector(".turn-name-display");
 
-// const tableTopBoxes = document.querySelectorAll('.table-top')
-// const tableMidBoxes = document.querySelectorAll('.table-mid')
-// const tableEndBoxes = document.querySelectorAll('.table-end')
-
 // You hover over it, it changes the background color
 // You hover away from it, it removes the background color
 // You click the box, it changes the background color to the designated color
 
-tdTags.forEach((tdTag) => {
-  tdTag.addEventListener("mouseover", (e) => {
-    // the ligthBlue and aliceBlue will need to be dynamic to the current player's color
-    if (!e.target.classList.contains('lightBlue')) {
-      e.target.classList.add('aliceBlue');
-    }
-  });
+function mouseOver(e) {
+  // TODO  the aliceBlue would represent the variable of current player
+  if (!e.target.classList.contains("lightBlue")) {
+    e.target.classList.add("aliceBlue");
+  }
+}
 
-//   the aliceBlue would represent the variable of current player
-  tdTag.addEventListener("mouseout", (e) => {
-    if (e.target.classList.contains('aliceBlue')) {
-      e.target.classList.remove('aliceBlue');
+function mouseOut(e){
+    // TODO  the aliceBlue would represent the variable of current player
+    if (e.target.classList.contains("aliceBlue")) {
+      e.target.classList.remove("aliceBlue");
     }
-  });
+}
+
+tdTags.forEach((tdTag) => {
+  tdTag.addEventListener("mouseover", mouseOver);
+  tdTag.addEventListener("mouseout", mouseOut);
 
   tdTag.addEventListener("click", (e) => {
     if (!e.target.classList.contains('lightBlue') || !e.target.classList.contains('lightOrange')) {
-        // change lightBlue to the current player's variable
+        // TODO change lightBlue to the current player's variable
       e.target.classList.add('lightBlue');
     }
   });
@@ -83,7 +81,7 @@ nameInputBox.addEventListener("keydown", (e) => {
     nameInputBox.disabled = true;
     nameInputBox.disabled = false;
 
-    // Will need to send the name to the win display function
+    // Will need to send the name to the name holder for win display and turn player display
     const playerOneName = nameInputBox.value;
     console.log(playerOneName);
   }
