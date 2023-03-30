096a7dbd26cfd487db9816af2a7fe8ca8e25e9ee
Kevin Barrios <kevinbarrios@ymail.com>
2023-03-30 15:25:51 -0500

    hover color working. Need to change to orange for player two

--- script.js
+++ script.js
@@ -4,29 +4,29 @@ const apricot = "apricot";
 const mainBlue = "mainBlue";
 const mainOrange = "mainColor";
 
-const bgLightOne = "#F0F8FF"
+const bgLightOne = "#F0F8FF";
 const bgLightTwo = "#FFF6EC";
 const bgMainOne = "#B6DBFC";
 const bgMainTwo = "#F8C28E";
 
 let playerOne = {
   name: "" || "Player One",
-  mainColor: mainBlue,
-  secondaryColor: aliceBlue,
+  mainColor: "#B6DBFC",
+  secondaryColor: "#F0F8FF",
   value: "X",
   wins: 0,
 };
 
 let playerTwo = {
   name: "" || "Player Two",
-  mainColor: mainOrange,
-  secondaryColor: apricot,
+  mainColor: "#F8C28E",
+  secondaryColor: "#FFF6EC",
   value: "O",
   wins: 0,
 };
 
-let currentPlayer = playerOne; // starts game with player one
-// let currentPlayer = playerTwo; // starts game with player two
+// let currentPlayer = playerOne; // starts game with player one
+let currentPlayer = playerTwo; // starts game with player two
 
 // Header
 let nameDisplay = document.querySelector(".player-name");
@@ -35,8 +35,9 @@ nameDisplay.innerText = currentPlayer.name;
 const newGameButton = document.querySelector(".new-game-button");
 newGameButton.addEventListener("click", () => {
   tdTags.forEach((tdTag) => {
-    tdTag.classList.remove("mainBlue");
-    tdTag.classList.remove("mainOrange");
+    // tdTag.classList.remove("bgMainOne");
+    // tdTag.classList.remove("bgMainTwo");
+    tdTag.style.background = "white";
     currentPlayer = playerOne;
   });
 });
@@ -69,37 +70,40 @@ if (blue className count > orange className count){
 
 /*
 let playerOne = {
-  mainColor: mainBlue,
-  secondaryColor: aliceBlue,
-  value: "X",
+  mainColor: "#B6DBFC",
+  secondaryColor: "#F0F8FF" || bgLightOne
 };
 
 let playerTwo = {
-  mainColor: mainOrange,
-  secondaryColor: apricot,
-  value: "O",
+  mainColor: "#F8C28E",
+  secondaryColor: "#FFF6EC" || bgLightTwo
 };
 */
 
 function mouseOver(e) {
-  // TODO  the aliceBlue would represent the variable of current player ***
-  if (currentPlayer === playerOne &&
-    (!e.target.classList.contains(currentPlayer.mainColor) ||
-    !e.target.classList.contains(playerOne.secondaryColor) ||
-    e.target.classList.contains(playerTwo.secondaryColor))
-  ) {
-    e.target.classList.add(playerOne.secondaryColor); // change to currentPlayer.secondaryColor
+  // TODO  the bgLightOne would represent the variable of current player ***
+  if ((currentPlayer === playerOne && !e.target.style.backgroundColor !== playerOne.secondaryColor) ||
+    e.target.style.backgroundColor !== playerTwo.secondaryColor) {
+    e.target.style.backgroundColor = playerOne.secondaryColor; // change to currentPlayer.secondaryColor
   } else {
-    e.target.classList.add("apricot");
+    e.target.style.backgroundColor = playerTwo.secondaryColor;
   }
 }
 
 function mouseOut(e) {
-  // TODO  the aliceBlue would represent the variable of current player ***
-  if (e.target.classList.contains("aliceBlue")) {
-    e.target.classList.remove("aliceBlue");
-  } else if (e.target.classList.contains("apricot")) {
-    e.target.classList.remove("apricot");
+  // TODO  the bgLightOne would represent the variable of current player ***
+  // if (e.target.classList.contains("bgLightOne")) {
+  //   e.target.classList.remove("bgLightOne");
+  // } else if (e.target.classList.contains("bgLightTwo")) {
+  //   e.target.classList.remove("bgLightTwo");
+  // }
+
+  // if (e.target.style.backgroundColor === playerOne.secondaryColor || e.target.style.backgroundColor === playerTwo.secondaryColor) {
+  //   e.target.style.backgroundColor = "white";
+  // }
+
+  if(e.target.style.backgroundColor !== 'white'){
+    e.target.style.backgroundColor = 'white'
   }
 }
 
@@ -108,16 +112,17 @@ tdTags.forEach((tdTag) => {
   tdTag.addEventListener("mouseout", mouseOut);
 
   tdTag.addEventListener("click", (e) => {
-    if (currentPlayer === playerOne &&
+    if (
+      currentPlayer === playerOne &&
       (!e.target.classList.contains(playerOne.mainColor) ||
-      !e.target.classList.contains(playerTwo.mainColor)) &&
-      (!e.target.classList.contains(playerOne.secondaryColor))
+        !e.target.classList.contains(playerTwo.mainColor)) &&
+      !e.target.classList.contains(playerOne.secondaryColor)
     ) {
-      // TODO change mainBlue to the current player's variable ***
+      // TODO change bgMainOne to the current player's variable ***
       // if(currentPlayer === playerOne){
-        e.target.classList.add("mainBlue"); // currentPlayer.mainColor
-      } else {
-        e.target.classList.add('mainOrange')
+      e.target.classList.add("bgMainOne"); // currentPlayer.mainColor
+    } else {
+      e.target.classList.add("bgMainTwo");
       // }
     }
     switchPlayer();
@@ -131,16 +136,16 @@ nameInputBoxes.forEach((nameInputBox) => {
     e.target.placeholder = "";
   });
 
-  nameInputBox.addEventListener('input', (e) => {
-    console.log(e)
-    e.preventDefault()
-    if(e.target.id === "player-one"){
-      playerOne.name = e.target.value
+  nameInputBox.addEventListener("input", (e) => {
+    console.log(e);
+    e.preventDefault();
+    if (e.target.id === "player-one") {
+      playerOne.name = e.target.value;
     } else {
-      playerTwo.name = e.target.value
+      playerTwo.name = e.target.value;
     }
-    playerName = e.target.value || e.target.placeholder
-  })
+    playerName = e.target.value || e.target.placeholder;
+  });
   // TODO When focus is removed from the input boxes, how to save? ***
 
   nameInputBox.addEventListener("blur", (e) => {
@@ -172,10 +177,8 @@ nameInputBoxes.forEach((nameInputBox) => {
   });
 });
 
-
 // TODO Add event listener to clear out the number of wins and ties back to zero ***
 const resetScoreButton = document.querySelector(".reset-score-button");
-
 
 // !DELETE *************************************************
 let object = document.createElement("button");
